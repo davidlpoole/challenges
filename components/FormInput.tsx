06f934643f61851ec0a71a578f3354cd8f978193
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-10 21:09:35 +1300

    Output full employee object

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -8,7 +8,7 @@ export default function FormInput(props: Props) {
     <input
       id={props.name}
       name={props.name}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
+      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors w-40"
       type="text"
       placeholder={props.placeholder}
       required
